940105d5be31fbf58f210ecd9ac2bb1283893eba
DaviLucasBorges <davilucasborges2010@gmail.com>
2026-05-07 09:42:15 -0300

    Missão_02

---
+++ 2-Bimestre/TypeScript/07.05.2026/Q02.ts
@@ -0,0 +1,23 @@
+/* 
+
+Davi Lucas Borges
+2° DS A 
+
+07.05.2026
+
+Questão 02
+
+*/
+
+// Nasci no dia 20
+
+let sinal: number = 45;
+
+if(sinal > 45)
+{
+    console.log("\nO Seu Sinal está Instável");
+}
+else
+{
+    console.log("\nO Seu Sinal está Estável");
+}
