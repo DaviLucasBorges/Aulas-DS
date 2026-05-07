a86efefc5e3b9008bc684908d18a7d9575487ab7
DaviLucasBorges <davilucasborges2010@gmail.com>
2026-05-07 09:39:55 -0300

    Missão_01

---
+++ 2-Bimestre/TypeScript/07.05.2026/Q01.ts
@@ -0,0 +1,17 @@
+/* 
+
+Davi Lucas Borges
+2° DS A 
+
+07.05.2026
+
+Questão 01
+
+*/
+
+let agente: string = "Davi";
+
+let setor: number = 2;
+
+console.log(`\nSeja Bem vindo ${agente}`);
+console.log(`\nO seu setor Atual é o setor ${setor}\n`);
