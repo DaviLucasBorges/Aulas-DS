65178b67ac95560b36132a2e418f2acc3e3be119
DaviLucasBorges <davilucasborges2010@gmail.com>
2026-05-07 09:43:21 -0300

    Missão_02

---
+++ 2-Bimestre/TypeScript/07.05.2026/Q03.ts
@@ -0,0 +1,20 @@
+/* 
+
+Davi Lucas Borges
+2° DS A 
+
+07.05.2026
+
+Questão 03
+
+*/
+
+console.log("\nÈ Declarado a Constante Core que é 10")
+
+const core = 10
+
+console.log("\nO valor é Alterado Para 20")
+
+const core = 20
+
+console.log("\nERRO !!! , Uma Constante Não Pode Ser Alterada")
